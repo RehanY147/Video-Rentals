67e4e8dda9a6a24ac9fbc90da935723cbf9b8ed0
shikari\rehan <rehanyousaf147@gmail.com>
2018-10-12 01:53:47 +0500

    Add Rental to Models
    Add Rental to Models

--- FoodOrder/Models/ApplicationDbContext.cs
+++ FoodOrder/Models/ApplicationDbContext.cs
@@ -16,6 +16,8 @@ namespace FoodOrder.Models
         public DbSet<MembershipType> MembershipTypes { get; set; }
 
         public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<Rental> Rentals { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
